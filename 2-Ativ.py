a18c4b70ee4e3dffa459016541ef0d8bc023b2b6
Poglonesbr <131016905+Poglonesbr@users.noreply.github.com>
2024-02-29 17:28:05 +0000

    2 Atividade

---
+++ 2-Ativ.py
@@ -0,0 +1,11 @@
+A = 10
+B = 5
+
+print(" ", A - B, "\n", A / B, "\n", A + B)
+
+C = input("Digite o valor de C: ")
+D = input("Digite o valor de D: ")
+E = C
+C = D
+D = E
+print("O valor de C é: ", C, " e o valor de D é: ", D)
