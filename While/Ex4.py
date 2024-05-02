4163085e40112877ad2a3e782c75a8337d79eaeb
Poglonesbr <131016905+Poglonesbr@users.noreply.github.com>
2024-05-02 17:35:54 +0000

    While

---
+++ While/Ex4.py
@@ -0,0 +1,5 @@
+y = 1
+while y <= 10:
+    x = float(input("Digite o valor de um numero: "))
+    print( x / 2 )
+    y += 1
---
+++ While/Ex5.py
@@ -0,0 +1,5 @@
+y = 1
+while y <= 5:
+    x = float(input("Digite o valor de um numero: "))
+    print(x ** 3)
+    y += 1
