feb4a6a925a39ba8d3023c0b7d33ae06dcaeab22
Poglonesbr <131016905+Poglonesbr@users.noreply.github.com>
2024-05-30 15:25:09 +0000

    Terminei

--- For/Ex4.py
+++ For/Ex4.py
@@ -1,7 +1,12 @@
-resto = 0
-primo = 0
-for i in range(0, 10):
-    if num % i == 0:
-         resto += 1
-         primo
-    
+numprimo = 0
+num = 2
+
+while numprimo < 10:
+    primo = True
+    for i in range(2, int(num**0.5) + 1):  
+        if num % i == 0:
+            primo = False
+    if primo:
+        print(f"O número {num} é primo!")
+        numprimo += 1
+    num += 1
