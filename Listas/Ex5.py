f6d53e1055be1b770fbd1faed4070ed3c1789784
Poglonesbr <131016905+Poglonesbr@users.noreply.github.com>
2024-07-11 23:26:46 +0000

    Finalizei

--- Listas/Ex5.py
+++ Listas/Ex5.py
@@ -4,13 +4,17 @@ maior = x[0]
 nmaior = 0
 menor = x[0]
 nmenor = 0
+
 for i in range (1, 10):
+    x[i] = int(input("Digite um numero: "))
     if maior < x[i]:
         maior = x[i]
         nmaior = i
+
     if menor > x[i]:
         menor = x[i]
         nmenor = i
-    x[i] = int(input("Digite um numero: "))
+
+print(x)
     
 print(f"O maior está na posicao {nmaior} e o menor esta na posicao {nmenor}")
